c8ecb8de0e9757f40e4ca6ac1fbea56a46836afa
Zinct2 <91075658+Zinct2@users.noreply.github.com>
2021-11-14 20:39:56 +0700

    Login

--- app/src/main/java/com/taufiq/fitfood/MainActivity.java
+++ app/src/main/java/com/taufiq/fitfood/MainActivity.java
@@ -1,6 +1,10 @@
 package com.taufiq.fitfood;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.login);
+        textView.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            startActivity(new Intent(getApplicationContext(), Login.class));
+        }
+    });
     }
 }
