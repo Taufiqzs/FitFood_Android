b39aa2ab8f513906a150facb1b997bf9e0b0f3c4
matthew-sw <matthewsebastian001@gmail.com>
2021-11-15 08:49:11 +0700

    Add Register

--- app/src/main/java/com/taufiq/fitfood/MainActivity.java
+++ app/src/main/java/com/taufiq/fitfood/MainActivity.java
@@ -21,5 +21,12 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(getApplicationContext(), Login.class));
         }
     });
+        TextView textView2 = findViewById(R.id.register);
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Register.class));
+            }
+        });
     }
 }
